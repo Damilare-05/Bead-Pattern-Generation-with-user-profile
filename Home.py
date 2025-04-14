779af8d3db1b0636158d9104f7ddd33c2774595e
Talabi Damilare Hannah <damilaretalabi9@gmail.com>
2025-04-14 10:55:10 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -131,7 +131,7 @@ def sign_up():
                     st.session_state["PASSWORD"] = create_password
                     st.success("Account Created")
                     # Switch page after successful sign-up.
-                    st.switch_page(r"main\pages\BeadPatternGeneraton.py")
+                    st.switch_page("BeadPatternGeneraton (3).py")
             else:
                 st.error("Passwords do not match.")
         else:
@@ -155,7 +155,7 @@ def main():
     
     with col1:
         st.image(
-            r"main\logo\ChatGPT Image Apr 12, 2025, 12_25_09 AM.png",
+            "ChatGPT Image Apr 11, 2025, 03_18_52 AM.png",
             width=200
         )
     
